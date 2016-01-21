8701cb77e4c0c532c6d61c821783887e27ab0c11
DenzelXIII <denzel_justin@hotmail.com>
2016-01-21 13:57:21 +0100

    ui

---
+++ InputUI.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class InputUI : MonoBehaviour
+{
+    public InputField productField;
+    public InputField pricePerItem;
+    public InputField landCode;
+
+
+	// Use this for initialization
+	void Start ()
+    {
+	
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+	
+	}
+}
